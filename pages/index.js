60c129e49e9e5b65d84c4a71b8f82efdf361eee6
windsdeath <windsdeath@gmail.com>
2020-07-31 00:16:58 +0900

    두번째 커밋

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Link from "next/link";
 import AppLayout from "./components/AppLayout";
-
+import Head from "next/head";
 const Home = () => {
     return (
         <>
+            <Head>
+                <link rel="stylesheet" href="">
+            </Head>
             <AppLayout>
                 <Link href="/about"><a>about</a></Link>
                 <div>Hello, next !</div>
